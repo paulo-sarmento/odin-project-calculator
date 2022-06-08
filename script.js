75afa3349239eeedb008dd86f2337ad112bfc6d2
Paulo Almeida <paulocomy1408@gmail.com>
2022-06-08 00:27:00 -0400

    feito css e começado refatoramento do código

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const display = document.querySelector('.display > span')
 const displayHistory = document.querySelector('.display-history > span')
 const digits = document.querySelectorAll('.digit')
-const operators = document.querySelectorAll('.operator')
+
 const backspace = document.querySelector('.backspace')
 
 let number = 0
@@ -12,18 +12,23 @@ let arr = []
 let arr2 = []
 let comma = ''
 
-const getInput = (event) => {
-  let digit = ''
-  if(event.type === 'click') {
-    digit = event.target
-  } else if (event.type === 'keydown') {
-    digit = document.querySelector(`div[data-key='${event.key}']`)
-  }
+const checkEventType = ({type, target, key}) => { //verifica o tipo de evento para retornar o mesmo tipo de input
+  if(type === 'click') return target
+  if(type === 'keydown') return document.querySelector(`div[data-key='${key}']`)
+}
+
+const toggleActive = (digit) => {
   digit.classList.add('active')
-  setTimeout(() => {
-    digit.classList.remove('active')
-  }, 100);
-  if(/comma/.test(digit.classList)) {
+  setTimeout(() => digit.classList.remove('active'), 100)
+}
+
+const getInput = (e) => {
+  const digit = checkEventType(e)
+  const digitClassList = digit.classList.value
+
+  toggleActive(digit)
+
+  if(/comma/.test(digitClassList)) {
     if(display.innerText == '') {
       display.innerText = `0${digit.innerText}`
       comma = '.'
@@ -33,17 +38,17 @@ const getInput = (event) => {
       comma = '.'
       arr.push(comma)
     }
-  } else if (/clear/.test(digit.classList)) {
+  } else if (/clear/.test(digitClassList)) {
     reset()
   } else if (number1 === 0) {
-    if(!/operator/.test(digit.classList) && !/backspace/.test(digit.classList)) { //se o dígito clicado não for igual um operator e nem igual ao backspace
+    if(!/operator/.test(digitClassList) && !/backspace/.test(digitClassList)) { //se o dígito clicado não for igual um operator e nem igual ao backspace
       if(arr.join('').replace(/\./, '').length < 16) { //verifica se a length do arr é menor que 16, desconsiderando o ponto
         showInput(digit)
         arr.push(digit.innerText)
         arr2 = arr.join('')
         number = arr2
       }
-    } else if (/backspace/.test(digit.classList)) {
+    } else if (/backspace/.test(digitClassList)) {
       let validation = arr.pop()
       if(validation === '.') {
         comma = ''
@@ -55,10 +60,10 @@ const getInput = (event) => {
         number = display.innerText.replace(/,/, '.')
       }
     }
-    else if(display.innerText !== '' && /operator/.test(digit.classList)) { //se o display não for vazio e o dígito clicado for um operator
+    else if(display.innerText !== '' && /operator/.test(digitClassList)) { //se o display não for vazio e o dígito clicado for um operator
       arr = []
       arr2 = []
-      if(!/equals/.test(digit.classList)) { //se o operador clicado não for o de igualdade
+      if(!/equals/.test(digitClassList)) { //se o operador clicado não for o de igualdade
         number1 = number
         number = 0
         comma = ''
@@ -73,7 +78,7 @@ const getInput = (event) => {
       }
     }
   } else if(number1 !== 0 && number2 !== 0) {
-    if(/equals/.test(digit.classList)) {
+    if(/equals/.test(digitClassList)) {
       let result = operate(operator, Number(number1), Number(number2))    
       if(String(result).replace(/\./, '').length > 16) { //verifica se a length do resultado da operação é maior que 16, desconsiderando o ponto
         displayHistory.innerText = `${number1.toString().replace(/\./, ',').slice(0, 17)} ${operator} ${number2.toString().replace(/\./, ',').slice(0, 17)}=`
@@ -84,23 +89,23 @@ const getInput = (event) => {
         display.innerText = String(result).replace(/\./, ',')
         number1 = String(result)
       }
-    } else if (/backspace/.test(digit.classList)) {
+    } else if (/backspace/.test(digitClassList)) {
       displayHistory.innerText = ''
-    } else if (/operator/.test(digit.classList) ) {
+    } else if (/operator/.test(digitClassList) ) {
       number2 = 0
       operator = digit.innerText
       display.innerText = ''
       displayHistory.innerText = `${number1.toString().replace(/\./, ',').slice(0, 17)} ${operator}`
     }
   } else if(number1 !== 0 && number2 === 0) {
-    if(!/operator/.test(digit.classList) && !/backspace/.test(digit.classList)) { //se o dígito clicado não for igual um operator e nem igual ao backspace
+    if(!/operator/.test(digitClassList) && !/backspace/.test(digitClassList)) { //se o dígito clicado não for igual um operator e nem igual ao backspace
       if(arr.join('').replace(/\./, '').length < 16) { //verifica se a length do arr é menor que 16, desconsiderando o ponto
         showInput(digit)
         arr.push(digit.innerText)
         arr2 = arr.join('')
         number = arr2
       }
-    } else if (/backspace/.test(digit.classList)) {
+    } else if (/backspace/.test(digitClassList)) {
       let validation = arr.pop()
       if(validation === '.'){
         comma = ''
@@ -112,10 +117,10 @@ const getInput = (event) => {
         number = display.innerText.replace(/,/, '.')
       }
     }
-    else if(display.innerText !== '' && /operator/.test(digit.classList)) { //se o display não for vazio e o dígito clicado for um operator
+    else if(display.innerText !== '' && /operator/.test(digitClassList)) { //se o display não for vazio e o dígito clicado for um operator
       arr = []
       arr2 = []
-      if(!/equals/.test(digit.classList)) { //se o operador clicado não for o de igualdade
+      if(!/equals/.test(digitClassList)) { //se o operador clicado não for o de igualdade
         number2 = number
         if(operator === '/' && number == 0) {
           let result = operate(operator, Number(number1), Number(number2))
@@ -187,11 +192,8 @@ const add = (n1, n2) => n1 + n2
 const subtract = (n1, n2) => n1 - n2
 const multiply = (n1, n2) => n1 * n2
 const divide = (n1, n2) => {
-  if(n2 === 0) {
-    return alert('Não é possível dividir por zero')
-  } else {
-    return n1 / n2
-  }
+  if(n2 === 0) return alert('Não é possível dividir por zero')
+  return n1 / n2
 }
 
 const operate = (operator, n1, n2) => {
